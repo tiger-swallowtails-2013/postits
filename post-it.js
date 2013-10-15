4db9e477359196f5ee4d919de507e88814f7be31
Dev Bootcamp <Apprentice@dbc34.local>
2013-10-15 15:09:25 -0700

    first commit with changes to make postits visible and get mouse coordinates

--- post-it.js
+++ post-it.js
@@ -1,21 +1,49 @@
-var Board = function( selector ) {
-  // Your board related code goes here
-  
-  // Use $elem to access the DOM element for this board
-  var $elem = $( selector );
-  
-  function initialize() {
-    // What needs to happen when this object is created?
-  };
+// var Board = function( selector ) {
+//   // Your board related code goes here
 
-  initialize();
-};
+//   // Use $elem to access the DOM element for this board
+//   var $elem = $( selector );
 
-var PostIt = function() {
-  // Your post-it related code goes here
-};
+//   function initialize() {
+//     // What needs to happen when this object is created?
+//   };
 
-$(function() {
-  // This code will run when the DOM has finished loading
-  Board.new('#board');
-});
+//   initialize();
+// };
+
+var changePostItPos = function() {
+  $(".post-it").css("background-color","green")
+  console.log('yes')
+}
+
+
+var showPostIt = function() {
+  $(".post-it").show("blind")
+}
+
+
+
+// var postIt = function() {
+//   $('body').click(showPostIt)
+// };
+
+
+
+var giveCoords = function () {
+  $(document).click(function(e) {
+    var offset = $(this).offset();
+    $('#x_axis').html(e.clientX);
+    $('#y_axis').html(e.clientY);
+  });
+}
+
+$(document).ready(changePostItPos())
+
+$(document).ready(giveCoords)
+
+
+
+// $(function() {
+//   // This code will run when the DOM has finished loading
+//   Board.new('#board');
+// });
