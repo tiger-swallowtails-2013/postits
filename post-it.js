f17bc869795cb84623216047ae5386bc9b762f10
Dev Bootcamp <Apprentice@dbc34.local>
2013-10-15 17:40:41 -0700

    postits created and draggable

--- post-it.js
+++ post-it.js
@@ -11,13 +11,20 @@
 //   initialize();
 // };
 
-var changePostItPos = function() {
-  $(".post-it").css("background-color","green")
-}
+// var PostIt = function() {
+//   // Your post-it related code goes here
+// };
 
 
-var showPostIt = function() {
-  $(".post-it").show("blind")
+
+var changePostItPos = function(id,x,y) {
+  $("#"+id).css("top",y);
+  $("#"+id).css("left",x);
+};
+
+
+var showPostIt = function(id) {
+  $("#"+id).show("blind")
 }
 
 
@@ -28,32 +35,54 @@ var showPostIt = function() {
 
 
 
-var giveCoords = function () {
+
+var increment = {
+  value: 0,
+  count: function() {
+    this.value = this.value + 1
+
+  }
+
+}
+
+function createPostIt() {
+
   $(document).click(function(e) {
-    var offset = $(this).offset();
-    $('#x_axis').html(e.clientX);
-    $('#y_axis').html(e.clientY);
-  });
-}
-
-function appendDiv(id) {
-  console.log(id)
-  $(document).click(function() {
-    console.log(id)
-    $("#board").append("<div id='" + id + "' class='post-it'>")
-    ////.text("sticky")
+    // if e.clientX and e.clientY are inside a div with post-it class
+    //   then go into edit mode
+    // else
+    if (($(e.target).hasClass("post-it")) === false) {
+      id = increment.value
+      $("#board").append("<div id='" + id + "' class='post-it' contenteditable='true'>")
+      $("#"+id).append("<div class='post-it'>")
+      $("#"+id).draggable()
+      $("#"+id).html("<div class='header'> X </div>what?!"+id)
+      changePostItPos(id,e.clientX,e.clientY)
+      showPostIt(id)
+      increment.count()
+    }
   })
-  return (id + 1)
 }
-
-$(document).ready(changePostItPos())
-
-$(document).ready(giveCoords)
 
 $(document).ready(function(){
-  var id = 0;
-  id = appendDiv(id)
+  // loadOldPostIts()
+  // addCreateNewListeners()
+  createPostIt()
 })
+
+// function loadOldPostIts(){
+//   //some json request
+//   var postItJson = //return value of json request >> [{x:234,y:27,text:"akjshfdklwej"},{x:234,y:27,text:"akjshfdklwej"},{x:234,y:27,text:"akjshfdklwej"},{x:234,y:27,text:"akjshfdklwej"},{x:234,y:27,text:"akjshfdklwej"},{x:234,y:27,text:"akjshfdklwej"},{x:234,y:27,text:"akjshfdklwej"}]
+//   $.each(postItJson, function(singlePostIt){
+//     new PostIt(singlePostIt.x, singlePostIt.y, singlePostIt.text, id:singlePostIt.id)
+//   })
+// }
+
+// function addCreateNewListeners(){
+//   $('#board').click(function(event){
+//     new PostIt(event.offsetX, event.offsetY, "")
+//   })
+// }
 
 // $(function() {
 //   // This code will run when the DOM has finished loading
